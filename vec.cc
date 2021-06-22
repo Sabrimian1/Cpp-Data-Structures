6b87e4ec791f00052c4380b1b53d5abb9436b766
Stephan Abrimian <stephanabrimian@Stephans-MacBook-Pro.local>
2021-06-22 15:51:07 -0400

    implemented seperate compilation

--- vec.cc
+++ vec.cc
@@ -1,50 +1,15 @@
 #include <iostream>
-
-
-namespace sab{
-    template<Typename T>
-    class Vector{
-        public:
-            Vector(){
-                ptr = new T[1];
-                capacity = 1;
-                current = 0;
-            };
-
-            void push_back(T inst){
-                if (current == capacity){
-                    T* temp = new T[capacity * 2]
-                    for(int i = 0; i < capacity; i++){
-                        temp[i] = ptr[i];
-                    }
-
-                    delete [] ptr;
-                    capacity *= 2;
-                    ptr = temp;
-                }
-
-                ptr[current] = inst;
-                current++;
-            };
-
-            void get_total{
-                std::cout << current << std::endl;
-            };
-
-
-
-
-        private:
-            T* ptr;
-
-            int capacity;
-
-            int current;
-    }
-
-
-}
+#include "vec.hh"
 
 int main(){
+    sab::Vector<int> v;
 
+    v.push_back(7);
+
+    v.push_back(10);
+
+    v.get_total();
+
+    v.print();
+    
 }
---
+++ vec.hh
@@ -0,0 +1,64 @@
+#pragma once
+#include <iostream>
+
+
+
+namespace sab{
+
+    template<typename T>
+    class Vector{
+        public:
+            Vector(){
+                ptr = new T[1];
+                capacity = 1;
+                current = 0;
+            };
+
+            void push_back(T inst){
+                if (current == capacity){
+                    T* temp = new T[capacity * 2];
+                    for(int i = 0; i < capacity; i++){
+                        temp[i] = ptr[i];
+                    }
+
+                    delete [] ptr;
+                    capacity *= 2;
+                    ptr = temp;
+                }
+
+                ptr[current] = inst;
+                current++;
+            };
+
+
+
+            void get_total(){
+                std::cout << current << std::endl;
+            };
+
+            void get_from_index(int index){
+                if (index < current){
+                    std::cout<< ptr[index] << std::endl;
+                }
+            }
+
+            void print(){
+                for (int i = 0; i < current; i++){
+                    std::cout << ptr[i] << std::endl;
+                }
+            }
+
+
+
+
+        private:
+            T* ptr;
+
+            int capacity;
+
+            int current;
+    };
+
+
+};
+
