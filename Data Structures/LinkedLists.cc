428dd200931d2c7d22d1165eff80461367683745
Stephan Abrimian <stephanabrimian@Stephans-MacBook-Pro.local>
2021-07-06 16:15:59 -0400

    simple linked list implementation

--- Data Structures/LinkedLists.cc
+++ Data Structures/LinkedLists.cc
@@ -1,6 +1,56 @@
 #include <iostream>
+#include <vector>
 
+
+template<typename T>
+class Node{
+    public:
+        T data;
+        Node<T>* next;
+        Node<T>* prev;
+
+
+};
 
 int main(){
+
+    Node<int>* head = new Node<int>();
+    Node<int>* second = new Node<int>();
+    Node<int>* third = new Node<int>();
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 10;
+    second->next = third;
+
+
+    third->data = 100;
+    third->next = nullptr;
+
+
+    std::vector<Node<int>*> myVec;
+
+
+    myVec.push_back(head);
+    myVec.push_back(second);
+    myVec.push_back(third);
+
+    for(int i = 0; i < myVec.size(); i++){
+        std::cout << myVec[i]->data << std::endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
     
+
+
 }
